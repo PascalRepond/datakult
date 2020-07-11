c6c61963df6f7c97ed39f56be30c40da93d168bc
DonPascualino <50177009+DonPascualino@users.noreply.github.com>
2020-07-11 17:30:10 +0200

    env variables

--- datakult_project/settings.py
+++ datakult_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j03%v0s6zu#nmf564)dva0)qg2vibb*8e#1!c%65ghx)(9ym^#'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = []
 
