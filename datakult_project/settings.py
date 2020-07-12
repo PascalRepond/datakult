763b032d2299df3c025b4b9915f2a395467d8042
DonPascualino <50177009+DonPascualino@users.noreply.github.com>
2020-07-12 11:46:28 +0200

    account: mail service and password management

--- datakult_project/settings.py
+++ datakult_project/settings.py
@@ -163,7 +163,13 @@ AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend',
 )
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
 
 ACCOUNT_SESSION_REMEMBER = True
 
@@ -187,3 +193,5 @@ SOCIALACCOUNT_PROVIDERS = {
         }
     }
 }
+
+DEFAULT_FROM_EMAIL = 'admin@datakult.com'
