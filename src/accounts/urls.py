960396785e1133e15b517908d3c17868819ec922
Pascal Repond <pascal.repond@rero.ch>
2026-01-07 17:59:03 +0100

    fix: change language form
    - Use a default Django view for set_language instead of a custom one.
    - This resolves issues with language switching and ensures proper redirection.

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.i18n import set_language
 
 from . import views
 
@@ -8,5 +9,5 @@ urlpatterns = [
     path("profile/", views.profile_edit, name="profile_edit"),
     path("profile/validate-profile/", views.validate_profile_field, name="validate_profile_field"),
     path("profile/validate-password/", views.validate_password_field, name="validate_password_field"),
-    path("set-language/", views.set_language, name="set_language"),
+    path("set-language/", set_language, name="set_language"),
 ]
